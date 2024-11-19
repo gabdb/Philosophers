f1ccfb003158fa5e2a8b8ab5f6c86596959637ce
Gabriel Nyssens <gnyssens@fu-r2-p6.s19.be>
2024-11-19 18:36:12 +0100

    jsp

--- philo.h
+++ philo.h
@@ -6,12 +6,25 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:55:41 by gnyssens          #+#    #+#             */
-/*   Updated: 2024/11/12 14:56:32 by gnyssens         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:56:52 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//ifndef etc.
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+
+typedef struct s_philo
+{
+	pthread_t		thread;
+	pthread_mutex_t	left_fork;
+	pthread_mutex_t	right_fork;
+	
+	int				meals;
+}               t_philo;
 
