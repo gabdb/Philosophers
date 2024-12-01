60da61d91282278b77e1abf6e2de499b55cbbe21
gabdb <gabrielnyssens@gmail.com>
2024-12-01 16:52:37 +0100

    jsp trop, tester sur machines diff

--- routine.c
+++ routine.c
@@ -6,7 +6,7 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 16:13:09 by gnyssens          #+#    #+#             */
-/*   Updated: 2024/11/30 23:43:57 by gnyssens         ###   ########.fr       */
+/*   Updated: 2024/12/01 16:50:15 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int	all_meals_eaten(t_data *data)
 void	*check_dead(void *arg)
 {
 	t_data	*data;
+	int		nb;
 
 	data = (t_data *)arg;
 	while (all_meals_eaten(data) == 0)
@@ -45,8 +46,22 @@ void	*check_dead(void *arg)
 			pthread_mutex_unlock(&data->print_mutex);
 			return (NULL);
 		}
-		
-		//ft_usleep(1); //jsp si necessaire, pr eviter de surchauffer
+		nb = -1;
+		while (++nb < data->number_philos)
+		{
+			if (!data->philos[nb].is_eating && get_time_ms() - data->philos[nb].last_meal_time > data->time_to_die)
+			{
+			pthread_mutex_lock(&data->death_mutex); //p e enlever ce mutex
+			data->someone_dead = 1;
+			data->dead_id = data->philos[nb].id;
+			pthread_mutex_unlock(&data->death_mutex);
+			pthread_mutex_lock(&data->print_mutex);
+			printf("%lld %d is dead\n", get_time_ms() - data->start_time, data->dead_id);
+			pthread_mutex_unlock(&data->print_mutex);
+			return (NULL);
+			}
+		}
+		usleep(10); //jsp si necessaire, pr eviter de surchauffer
 	}
 	return (NULL);
 }
@@ -65,26 +80,28 @@ void	*routine(void *arg)
 	while (1)
 	{
 		//check death
-		pthread_mutex_lock(&data->death_mutex);
+		//pthread_mutex_lock(&data->death_mutex);
         if (data->someone_dead)
         {
-            pthread_mutex_unlock(&data->death_mutex);
+            //pthread_mutex_unlock(&data->death_mutex);
             break;
         }
-        pthread_mutex_unlock(&data->death_mutex);
-		if (get_time_ms() - philo->last_meal_time  + data->time_to_eat > data->time_to_die)
+        //pthread_mutex_unlock(&data->death_mutex);
+		
+		if (get_time_ms() - philo->last_meal_time > data->time_to_die) //+ data->time_to_eat ?
 		{
 			philo->is_dead = 1;
-			pthread_mutex_lock(&data->death_mutex); //p e enlever ce mutex
+			//pthread_mutex_lock(&data->death_mutex); //p e enlever ce mutex
 			data->someone_dead = 1;
 			data->dead_id = philo->id;
-			pthread_mutex_unlock(&data->death_mutex);
+			//pthread_mutex_unlock(&data->death_mutex);
 			break;
 		}
+		
 		if (data->number_meals != -1 && philo->meals_eaten >= data->number_meals)
 			break;
 		if (philo->id % 2 == 0)
-			usleep(50); //sleep 0.5 ms to avoid bug
+			usleep(350); //sleep a bit to avoid bug
 		lf = philo->left_fork;
 		pthread_mutex_lock(data->forks + lf);
 		if (data->someone_dead)
@@ -102,17 +119,17 @@ void	*routine(void *arg)
 		
 		//now that philo has 2 forks, and can eat
 		
-		philo->is_eating = 1; //SERT A RIEN
+		philo->is_eating = 1; //SERT A RIEN ?
 		if (data->someone_dead)
 			break;
 		pthread_mutex_lock(&data->print_mutex);
 		printf("%lld %d is eating\n", get_time_ms() - data->start_time, philo->id);
+		philo->last_meal_time = get_time_ms();
 		pthread_mutex_unlock(&data->print_mutex);
 		ft_usleep(data->time_to_eat);
 		pthread_mutex_unlock(data->forks + lf);
 		pthread_mutex_unlock(data->forks + rf);
 		philo->is_eating = 0;
-		philo->last_meal_time = get_time_ms();
 		philo->meals_eaten += 1;
 		
 		//philo has released forks and will sleep
